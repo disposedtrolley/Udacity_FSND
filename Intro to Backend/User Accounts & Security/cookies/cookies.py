c7a2b88cb01728c7b861319126995cd39b5c20f8
James Liu <disposedtrolley@gmail.com>
2017-01-16 14:23:25 +1100

    moved activity files and added cookies exercise

---
+++ Intro to Backend/User Accounts & Security/cookies/cookies.py
@@ -0,0 +1,64 @@
+import os
+import hmac
+import jinja2
+import webapp2
+
+template_dir = os.path.dirname(__file__)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+SECRET = "imsosecret"
+
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        visits = 1
+        visit_cookie_str = self.request.cookies.get('visits')
+        # if the cookie exists, check if value matches the hashed value
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            # if value matches, cast to int and increment visits
+            if cookie_val:
+                visits = int(cookie_val)
+                visits += 1
+
+        # make new cookie string consisting of number and hashed value
+        new_cookie_val = make_secure_val(str(visits))
+
+        # update the cookie with the new visit count
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+
+        # display different content depending on number of visits
+        if visits > 20:
+            self.write("You are the best ever!")
+        else:
+            self.write("You've been here %s times!" % visits)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+    ],
+    debug=True)
