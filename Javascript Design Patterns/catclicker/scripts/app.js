62080fe9b355f604fa522113b69c2fcf0862c544
James Liu <disposedtrolley@gmail.com>
2017-02-11 15:22:39 +1100

    added admin view

--- Javascript Design Patterns/catclicker/scripts/app.js
+++ Javascript Design Patterns/catclicker/scripts/app.js
@@ -5,16 +5,19 @@ $(function() {
         init: function() {
             this.cats = [
                 {
+                    id: 1,
                     name: 'Bella',
                     clickCount: 0,
                     imgSrc: '/img/Bella.jpg'
                 },
                 {
+                    id: 2,
                     name: 'Tigger',
                     clickCount: 0,
                     imgSrc: '/img/Tigger.jpg'
                 },
                 {
+                    id: 3,
                     name: 'Oreo',
                     clickCount: 0,
                     imgSrc: '/img/Oreo.jpg'
@@ -22,10 +25,10 @@ $(function() {
             this.currentCat = this.cats[0];
         },
 
-        setCurrentCat: function(catName) {
+        setCurrentCat: function(catId) {
             for (var i = 0; i < this.cats.length; i++) {
                 var thisCat = this.cats[i];
-                if (thisCat.name === catName) {
+                if (thisCat.id === catId) {
                     this.currentCat = thisCat;
                 }
             }
@@ -39,11 +42,20 @@ $(function() {
         incrementClickCount: function() {
             for (var i = 0; i < this.cats.length; i++) {
                 var thisCat = this.cats[i];
-                if (thisCat.name === this.currentCat.name) {
+                if (thisCat.id === this.currentCat.id) {
                     thisCat.clickCount += 1;
                 }
             }
             catView.render();
+        },
+
+        updateCatDetails: function(cat) {
+            for (var i = 0; i < this.cats.length; i++) {
+                var thisCat = this.cats[i];
+                if (thisCat.id === cat.id) {
+                    this.cats[i] = cat;
+                }
+            }
         }
     };
 
@@ -52,24 +64,32 @@ $(function() {
             model.init();
             listView.init();
             catView.init();
+            adminView.init();
         },
 
         getAllCats: function() {
             return model.cats;
         },
 
-        getCurrentCat: function(catName) {
-            return model.getCurrentCat(catName);
+        getCurrentCat: function(catId) {
+            return model.getCurrentCat(catId);
         },
 
-        setCurrentCat: function(catName) {
-            model.setCurrentCat(catName);
+        setCurrentCat: function(catId) {
+            model.setCurrentCat(catId);
             catView.render();
+            adminView.init();
         },
 
         incrementClickCount: function() {
             model.incrementClickCount();
             catView.render();
+        },
+
+        updateCatDetails: function(cat) {
+            model.updateCatDetails(cat);
+            catView.render();
+            listView.render();
         }
     };
 
@@ -79,8 +99,8 @@ $(function() {
             this.$catList = $('.cat-names');
             // attach event listener to links
             this.$catList.on('click', '.cat-link', function(e) {
-                var catName = $(this).text();
-                controller.setCurrentCat(catName);
+                var catId = parseInt($(this).attr('id'));
+                controller.setCurrentCat(catId);
             });
             listView.render();
         },
@@ -90,7 +110,8 @@ $(function() {
             // populate cat list html into the container DOM node
             controller.getAllCats().forEach(function(cat) {
                 var catName = cat.name;
-                htmlStr += '<button type="button" class="list-group-item cat-link" id=' + catName + '>' + catName + '</button>';
+                var catId = cat.id;
+                htmlStr += '<button type="button" class="list-group-item cat-link" id=' + catId + '>' + catName + '</button>';
             });
             this.$catList.html(htmlStr);
         }
@@ -99,12 +120,18 @@ $(function() {
     var catView = {
         init: function() {
             // reference cat details
-            this.$catDetails = $('.cat-container');
+            var $catDetails = $('.cat-container');
             // attach event listener to button
-            this.$catDetails.on('click', 'button', function(e) {
+            $catDetails.on('click', 'img', function(e) {
                 controller.incrementClickCount();
             });
             catView.render();
+
+            var $adminButton = $('.show-admin');
+            $adminButton.on('click', function(e) {
+                adminView.render();
+                $('.admin-container').show();
+            });
         },
 
         render: function() {
@@ -121,6 +148,44 @@ $(function() {
             $('.cat-img').attr('src', imgSrc);
             // update click count
             $('.count').text(clickCount);
+        }
+    };
+
+    var adminView = {
+        init: function() {
+            this.cat = controller.getCurrentCat();
+
+            var $adminContainer = $('.admin-container');
+            $adminContainer.hide();
+
+            this.$inputCatName = $adminContainer.find('form > .form-group > #inputCatName');
+            this.$inputImageUrl = $adminContainer.find('form > .form-group > #inputImageUrl');
+            this.$inputClickCount = $adminContainer.find('form > .form-group > #inputClickCount');
+
+            var $saveButton = $adminContainer.find('.save-details');
+            $saveButton.on('click', function(e) {
+                e.preventDefault();
+                adminView.cat.name = adminView.$inputCatName.val();
+                adminView.cat.imgSrc = adminView.$inputImageUrl.val();
+                adminView.cat.clickCount = adminView.$inputClickCount.val();
+                controller.updateCatDetails(adminView.cat);
+                controller.setCurrentCat(adminView.cat);
+                $adminContainer.hide();
+            });
+
+            var $cancelButton = $adminContainer.find('.cancel');
+            $cancelButton.on('click', function(e) {
+                e.preventDefault();
+                $adminContainer.hide();
+            });
+        },
+
+        render: function() {
+            // populate form with details of the current cat
+            this.cat = controller.getCurrentCat();
+            this.$inputCatName.val(this.cat.name);
+            this.$inputImageUrl.val(this.cat.imgSrc);
+            this.$inputClickCount.val(this.cat.clickCount);
         }
     };
 
